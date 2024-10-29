47ae35458956032139aa66216e1f8c8b669b5126
Vlad Margaritescu <92854906+VladMRG2001@users.noreply.github.com>
2024-10-29 15:53:53 +0200

    Add files via upload

--- POO_05/diriginte.hpp
+++ POO_05/diriginte.hpp
@@ -5,21 +5,19 @@
 #include <cstring>
 using namespace std;
 
-class Diriginte{
-protected:
-	char *nume;
-	char grupa[7];
-public:
-	Diriginte(); // constructor fara parametri
-	Diriginte(const char*, const char []); // constructor cu parametri
-	Diriginte(const Diriginte&); // contructor de copiere
-	Diriginte& operator=(const Diriginte&); // constructor de atribuire =
-	friend ostream& operator<<(ostream&, const Diriginte&); // constructor de afisare <<
-	void schimbare_nume(const char*); // metoda de schimbare nume
-	
-	~Diriginte(); // destructor
-	
-	
+class Diriginte {
+    protected:
+        char* nume;
+        char grupa[7];
+    public:
+        Diriginte();
+        Diriginte(const char*, const char[]);
+        Diriginte(const Diriginte&);
+        ~Diriginte();
+
+        Diriginte& operator = (const Diriginte&);
+        friend ostream& operator << (ostream&, const Diriginte&);
+        void schimbare_nume(const char*);
 };
 #endif
 
