0753ee39f4f11443a7f0834217542602dd28fd8f
Vlad Margaritescu <92854906+VladMRG2001@users.noreply.github.com>
2024-10-29 15:55:01 +0200

    Add files via upload

--- POO_05/nota.hpp
+++ POO_05/nota.hpp
@@ -4,17 +4,20 @@
 #include <iostream>
 using namespace std;
 
-class Nota{
-protected:
-	int *note;
-	int nr_note;
-public:
-	Nota(); // contructor fara parametri
-	Nota(const int*, int ); // constructor cu parametri
-	friend ostream& operator<<(ostream&, const Nota&); // constructor <<
-	Nota& operator=(const Nota&); // constructor =
-	int getMedie(); // metoda getter de medie
-	~Nota(); // destructor
+class Nota {
+    protected:
+        int *note;
+        int nr_note;
+    
+    public:
+        Nota(); // constructor fara parametri
+        Nota(const int*, int); // constructor cu parametri
+        Nota(const Nota&); // constructor de copiere
+        ~Nota(); // destructor
+
+        Nota& operator = (const Nota&); // operatorul de atribuire
+        friend ostream& operator << (ostream&, const Nota&);
+        float getMedie() const;
 };
 
 #endif
