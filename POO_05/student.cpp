dd972e5f8dd5fce319b3c97daf04d205c3e227e0
Vlad Margaritescu <92854906+VladMRG2001@users.noreply.github.com>
2024-10-29 15:55:26 +0200

    Add files via upload

--- POO_05/student.cpp
+++ POO_05/student.cpp
@@ -1,58 +1,47 @@
 #include "student.hpp"
 
-Student::Student():Nota(),Diriginte(),nume(NULL)
-{
+Student::Student(): nume(NULL) {}
+
+Student::Student(const int* note, int nr_note, const char* nume_diriginte, const char grupa[], int nr_elevi, const char* nume): Nota(note, nr_note), Diriginte(nume_diriginte, grupa), Clasa(nr_elevi) {
+    this->nume = new char[strlen(nume) + 1]; // doar param din Student aici
+    strcpy(this->nume, nume);
 }
 
-Student::Student(const int* note, int nr_note, const char* nume_d, const char grupa[], const char* nume):Nota(note,nr_note),Diriginte(nume_d,grupa)
-{
-
-	this->nume = new char[strlen(nume) + 1];
-	strcpy(this->nume,nume);
-
+Student::Student(const Student& obj) {
+    nume = NULL;
+    *this = obj;
 }
 
-Student::Student(const Student& obj)
-{
-	nume = NULL;
-	*this = obj;
-}
-
-Student& Student::operator=(const Student& obj)
-{
-	Diriginte::operator=(obj);
-	Nota::operator=(obj);
-	if (nume != NULL)
-		delete []nume;
-	this->nume = new char[strlen(obj.nume) + 1];
-	strcpy(this->nume,obj.nume);
-	
-	return *this;
-}
-
-ostream& operator<<(ostream& out, const Student& obj)
-{
-	out << (Diriginte&)obj;
-	out << (Nota&)obj;
-	if(obj.nume != NULL)
-		out << obj.nume;
-	else
-		out << "fara nume";
-	out << " ";
-	
+ostream& operator << (ostream& out, const Student& obj) {
+    out << (const Nota&) obj;
+	out << (const Diriginte&) obj;
+	out << (const Clasa&) obj;
+    if(obj.nume != NULL) {
+        out << "\nNume: " << obj.nume <<'\n' << '\n' ;
+    }
 	return out;
 }
 
-void Student::interschimbare(Student& obj)
-{
-	Student aux(*this);
-	*this = obj;
-	obj = aux;
+Student& Student::operator = (const Student& obj) {
+    Nota::operator = (obj);
+    Diriginte::operator = (obj);
+    Clasa::operator = (obj);
+    if (nume != NULL) {
+        delete[] nume;
+    }
+    this->nume = new char[strlen(obj.nume) + 1];
+    strcpy(this->nume, obj.nume);
+    return *this;
 }
 
-Student::~Student()
-{
-	delete []nume;
+void Student::interschimbare(Student& obj) {
+    Student aux(*this);
+    *this = obj;
+    obj = aux;
+}
+
+Student::~Student() {
+    delete[] nume;
 }
 
 
