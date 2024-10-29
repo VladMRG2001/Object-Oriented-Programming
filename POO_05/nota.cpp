8cb80cee21cce6a8a04f3baa5be9517822370a68
Vlad Margaritescu <92854906+VladMRG2001@users.noreply.github.com>
2024-10-29 15:54:41 +0200

    Add files via upload

--- POO_05/nota.cpp
+++ POO_05/nota.cpp
@@ -1,46 +1,55 @@
 #include "nota.hpp"
 
-Nota::Nota():note(NULL),nr_note(0){
+Nota::Nota() : note(NULL), nr_note(0) {} // pt int 0 si pt int* NULL
+
+Nota::Nota(const int* note, int nr_note) : nr_note(nr_note) {
+    this->note = new int[nr_note];
+    for (int i = 0; i < nr_note; i++) {
+        this->note[i] = note[i];
+    }
 }
-Nota::Nota(const int *note, int nr_note):nr_note(nr_note){
-	this->note=new int [nr_note];
-	for (int i=0; i<nr_note ; i++)
-	{
-		this->note[i]=note[i];
-	}
+
+Nota::Nota(const Nota& obj) : nr_note(obj.nr_note) {
+    this->note = new int[obj.nr_note];
+    for (int i = 0; i < obj.nr_note; i++) {
+        this->note[i] = obj.note[i];
+    }
 }
-Nota& Nota::operator=(const Nota& obj){
-	if (this->note!= NULL)
-		delete []this->note;
-	nr_note=obj.nr_note;
-	this->note = new int[obj.nr_note];
-	for(int i=0;i<nr_note;i++)
-		this->note[i]=obj.note[i];
+
+Nota& Nota::operator = (const Nota& obj) {
+    if(note != NULL) {
+        delete[] note;
+    }
+    nr_note = obj.nr_note;
+    this->note = new int[obj.nr_note];
+    for(int i = 0; i < nr_note; i++) {
+		this->note[i] = obj.note[i];
+    }
 	return *this;
 }
-ostream& operator<<(ostream& out, const Nota& obj)
-{
-	out << obj.nr_note << " ";
-	for(int i=0; i< obj.nr_note;i++)
-		out<<obj.note[i] << " ";
-	return out;
+
+ostream& operator << (ostream& out, const Nota& obj) {
+    out << "Nr de note: " << obj.nr_note << endl;
+    out << "Notele: ";
+    for (int i = 0; i < obj.nr_note; i++) {
+        out << obj.note[i] << " ";
+    }
+    out << "\nMedia: " << obj.getMedie() << '\n'; // Afișează media
+    return out;
 }
-int Nota::getMedie()
-{
-	int s=0;
-	float medie;
-	
-	for(int i=0; i<nr_note; i++)
-	{
-		s=s+note[i];
-	}
-	medie=(float)s/nr_note;
-	return medie;
+
+float Nota::getMedie() const{
+    float sum = 0;
+    for(int i = 0; i < nr_note; i++) {
+        sum = sum + note[i];
+    }
+    float media;
+    media = sum / nr_note;
+    return media;
 }
-	
-Nota::~Nota()
-{
-	delete []note;	
+
+Nota::~Nota() {
+    delete[] note;
 }
 	
 
