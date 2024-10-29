6fde0ba12424321586bc0d9eb7430c1450e2aa48
Vlad Margaritescu <92854906+VladMRG2001@users.noreply.github.com>
2024-10-29 15:53:31 +0200

    Add files via upload

--- POO_05/diriginte.cpp
+++ POO_05/diriginte.cpp
@@ -1,53 +1,50 @@
 #include "diriginte.hpp"
 
-Diriginte::Diriginte():nume(NULL)
-{
-	strcpy(this->grupa," ");
+Diriginte::Diriginte() : nume(NULL) { // pt char* NULL, pt char[] definit jos
+    strcpy(this->grupa, " ");
 }
 
-Diriginte::Diriginte(const char* nume, const char grupa[])
-{
-	this->nume = new char[strlen(nume)+1];
-	strcpy(this->nume,nume);
-	strcpy(this->grupa,grupa);
+Diriginte::Diriginte(const char* nume, const char grupa[]) {
+    this->nume = new char[strlen(nume) + 1];
+    strcpy(this->nume, nume);
+    strcpy(this->grupa, grupa);
 }
 
-Diriginte::Diriginte(const Diriginte& obj){
-	nume = NULL;
-	*this = obj;
+Diriginte::Diriginte(const Diriginte& obj) {
+    this->nume = new char[strlen(obj.nume) + 1];
+    strcpy(this->nume, obj.nume);
+    strcpy(this->grupa, obj.grupa);
 }
 
-Diriginte& Diriginte::operator=(const Diriginte& obj){
-	if (nume != NULL)
-		delete []nume;
-
-	nume = new char[strlen(obj.nume) + 1];
-	strcpy(nume,obj.nume);
-	strcpy(grupa,obj.grupa);
+Diriginte& Diriginte::operator = (const Diriginte& obj) {
+    if (nume != NULL) {
+		delete[] nume;
+    }
+	this->nume = new char[strlen(obj.nume) + 1];
+	strcpy(this->nume,obj.nume);
+	strcpy(this->grupa,obj.grupa);
 	return *this;
 }
 
-ostream& operator<<(ostream& out, const Diriginte& obj)
-{
-	if(obj.nume != NULL)
-		out << obj.nume;
-	else
-		out << "fara nume";
-	out << " ";
-	out << obj.grupa<< " ";
+ostream& operator << (ostream& out, const Diriginte& obj) {
+    if(obj.nume != NULL) {
+		out << "Numele dirigintelui: " << obj.nume; // asa pt char*
+    } else {
+		out << "Numele dirigintelui: " << "no name found x_x";
+    }
 	
+	out << "\nGrupa: " << obj.grupa<< " "; // asa pt char[]
+    out << '\n';
 	return out;
 }
 
-void Diriginte::schimbare_nume(const char* n)
-{
-	this->nume=new char[strlen(n) + 1];
-	strcpy(this->nume,n);
+void Diriginte::schimbare_nume(const char* n) {
+    this->nume = new char[strlen(n) + 1];
+	strcpy(this->nume, n);
 }
 
-Diriginte::~Diriginte()
-{
-	delete []nume;
+Diriginte::~Diriginte() {
+    delete[] nume;
 }
 
 
