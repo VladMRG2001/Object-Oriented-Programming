e6cb60d2ac4630dac080851894c785ea3a0b7f34
Vlad Margaritescu <92854906+VladMRG2001@users.noreply.github.com>
2024-10-29 15:52:38 +0200

    Add files via upload

--- POO_05/clasa.cpp
+++ POO_05/clasa.cpp
@@ -1,50 +1,23 @@
 #include "clasa.hpp"
 
-Clasa::Clasa() : nr_elevi(0), elevi(nullptr) {
-}
+Clasa::Clasa() : nr_elevi(1) {}
 
-Clasa::Clasa(int nr_elevi) : nr_elevi(nr_elevi) {
-    elevi = new Student[nr_elevi];
-}
+Clasa::Clasa(int nr_elevi) : nr_elevi(nr_elevi) {}
 
-Clasa::Clasa(Student* elevi, int nr_elevi) : nr_elevi(nr_elevi) {
-    this->elevi = new Student[nr_elevi]; // Alocă memorie pentru elevi
-    for (int i = 0; i < nr_elevi; ++i) {
-        this->elevi[i] = elevi[i]; // Copiază elevii
-    }
-}
+Clasa::Clasa(const Clasa& obj) : nr_elevi(obj.nr_elevi){}
 
-Clasa::Clasa(const Clasa& obj) : nr_elevi(obj.nr_elevi) {
-    elevi = new Student[nr_elevi];
-    for (int i = 0; i < nr_elevi; ++i) {
-        elevi[i] = obj.elevi[i];
-    }
-}
+Clasa::~Clasa() {}
 
-Clasa& Clasa::operator=(const Clasa& obj) {
-    if (this != &obj) {
-        delete[] elevi;
-        nr_elevi = obj.nr_elevi;
-        elevi = new Student[nr_elevi];
-        for (int i = 0; i < nr_elevi; ++i) {
-            elevi[i] = obj.elevi[i];
-        }
-    }
+Clasa& Clasa::operator = (const Clasa& obj) {
+    nr_elevi = obj.nr_elevi;
     return *this;
 }
 
-Clasa::~Clasa() {
-    delete[] elevi;
-}
-
 ostream& operator<<(ostream& out, const Clasa& obj) {
-    out << "Numar elevi: " << obj.nr_elevi << endl;
-    for (int i = 0; i < obj.nr_elevi; ++i) {
-        out << obj.elevi[i] << endl;
-    }
+    out << "Nr elevi: " << obj.nr_elevi; //la int e usor
     return out;
 }
 
-
-
-
+void Clasa::setNrelevi(int nr) {
+    nr_elevi = nr;
+}
